b69f81b308cab3b29a1e49c8dbe3c7ab7b503965
b4ldous <baldous@protonmail.com>
2022-10-10 09:25:12 -0500

    some styles were added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,56 @@
 import React, { useState } from "react";
-import useHackerNewsApi from './useHackerNewsApi'
-
-
+import useHackerNewsApi from "./useHackerNewsApi";
+import { Box, Button, Container, CssBaseline, List, Typography } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 const App = () => {
-  const [query, setQuery] = useState("redux");
+  const [query, setQuery] = useState("");
   const [{ data, isLoading, isError }, doFetch] = useHackerNewsApi();
   return (
-    <div>
-      <form
-        onSubmit={(event) => {
-          doFetch(`http://hn.algolia.com/api/v1/search?query=${query}`);
+    <>
+    <CssBaseline/>
+      <Container>
+        <Typography sx={{textAlign: "center", fontWeight: 1000}} variant="h2">Tech News</Typography>
+          
+        <Box sx={{mt: "50px", display: "flex", justifyContent: "center" }}>
+          <Box sx={{overflow: 'auto'}}>
+            <Box 
+            sx={{display: 'flex'}}
+            onSubmit={(event) => {
+            doFetch(`http://hn.algolia.com/api/v1/search?query=${query}`);
 
-          event.preventDefault();
-          setQuery("")
-        }}
-      >
-        <input
-          type="text"
-          value={query}
-          onChange={(event) => setQuery(event.target.value)}
-        />
-        <button type="submit">Search</button>
-      </form>
-      {isError && <div>Something went wrong ...</div>}
+            event.preventDefault();
+            setQuery("");
+          }} component="form">
+              
+              <Box
+              type="text"
+              value={query}
+              onChange={(event) => setQuery(event.target.value)}
+                placeholder="Search..."
+                sx={{flexGrow: 1,fontSize: "16px", borderRadius: "50px", padding: '15px' }}
+                component="input"
+              />
 
-      {isLoading ? (
-        <div>Loading ...</div>
-      ) : (
-        <ul>
-          {data.hits.map((item) => (
-            <li key={item.objectID}>
-              <a href={item.url}>{item.title}</a>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+<Button type="submit" sx={{ fontSize: "16px", borderRadius: "50px", padding: '15px' }}><SearchIcon/></Button>
+            </Box>
+          </Box>
+        </Box>
+       
+        {isError && <div>Something went wrong ...</div>}
+
+        {isLoading ? (
+          <div>Loading ...</div>
+        ) : (
+          <Box sx={{mt: "50px"}}>
+            {data.hits.map((item) => (
+              <List key={item.objectID}>
+                <Box sx={{textDecoration: "none"}} component="a" target='_blank' rel="noreferrer" href={item.url}>{item.title}</Box>
+              </List>
+            ))}
+          </Box>
+        )}
+      </Container>
+    </>
   );
 };
 
