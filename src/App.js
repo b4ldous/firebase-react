0a595b92c180156951d03a299e186c5138453a6e
b4ldous <baldous@protonmail.com>
2022-11-18 20:21:56 -0600

    some styles were added

--- src/App.js
+++ src/App.js
@@ -1,55 +1,97 @@
 import React, { useState } from "react";
 import useHackerNewsApi from "./useHackerNewsApi";
-import { Box, Button, Container, CssBaseline, List, Typography } from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
+import {
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  List,
+  Paper,
+  Typography,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 const App = () => {
   const [query, setQuery] = useState("");
   const [{ data, isLoading, isError }, doFetch] = useHackerNewsApi();
   return (
     <>
-    <CssBaseline/>
+      <CssBaseline />
       <Container>
-        <Typography sx={{textAlign: "center", fontWeight: 1000}} variant="h2">Tech News</Typography>
-          
-        <Box sx={{mt: "50px", display: "flex", justifyContent: "center" }}>
-          <Box sx={{overflow: 'auto'}}>
-            <Box 
-            sx={{display: 'flex'}}
-            onSubmit={(event) => {
-            doFetch(`https://hn.algolia.com/api/v1/search?query=${query}`);
+        <Typography sx={{marginTop:"50px", textAlign: "center", fontWeight: 1000 }} variant="h4">
+          Hacker News
+        </Typography>
 
-            event.preventDefault();
-            setQuery("");
-          }} component="form">
-              
+        <Box sx={{ mt: "50px", display: "flex", justifyContent: "center" }}>
+          <Box sx={{ overflow: "auto" }}>
+            <Box
+              sx={{ display: "flex" }}
+              onSubmit={(event) => {
+                doFetch(`https://hn.algolia.com/api/v1/search?query=${query}`);
+
+                event.preventDefault();
+                setQuery("");
+              }}
+              component="form"
+            >
               <Box
-              type="text"
-              value={query}
-              onChange={(event) => setQuery(event.target.value)}
+                type="text"
+                value={query}
+                onChange={(event) => setQuery(event.target.value)}
                 placeholder="Search..."
-                sx={{flexGrow: 1,fontSize: "16px", borderRadius: "50px", padding: '15px' }}
+                sx={{
+                  flexGrow: 1,
+                  fontSize: "16px",
+                  borderRadius: "50px",
+                  padding: "15px",
+                }}
                 component="input"
               />
 
-<Button type="submit" sx={{ fontSize: "16px", borderRadius: "50px", padding: '15px' }}><SearchIcon/></Button>
+              <Button
+                type="submit"
+                sx={{ fontSize: "16px", borderRadius: "50px", padding: "15px" }}
+              >
+                <SearchIcon />
+              </Button>
             </Box>
           </Box>
         </Box>
-       
-        {isError && <div>Something went wrong ...</div>}
+      </Container>
 
-        {isLoading ? (
-          <div>Loading ...</div>
-        ) : (
-          <Box sx={{mt: "50px"}}>
+      {isError && <div>Something went wrong ...</div>}
+
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <Container>
+          <Box sx={{ marginTop: "50px", marginBottom: "50px" }}>
             {data.hits.map((item) => (
-              <List key={item.objectID}>
-                <Box sx={{textDecoration: "none"}} component="a" target='_blank' rel="noreferrer" href={item.url}>{item.title}</Box>
-              </List>
+              
+                
+                <Box sx={{margin: "10px", padding: "10px", overflow: "auto"}} key={item.objectID}>
+                  <Box
+                    sx={{ textDecoration: "none", minWidth: "500px" }}
+                    component="a"
+                    target="_blank"
+                    rel="noreferrer"
+                    href={item.url}
+                  >
+                      <Box sx={{color: "black", marginBottom:"5px", whiteSpace:"nowrap"}}>{item.url}</Box>
+                      <Box sx={{color: "blue", typography:"h5"}}>{item.title}</Box>
+
+                     
+
+                    
+                  </Box>
+                  <Box sx={{typography: "body1"}}>Author: {item.author}</Box>
+                  <Box sx={{typography: "body2"}}> {item.created_at}</Box>
+                  
+                </Box>
+              
             ))}
           </Box>
-        )}
-      </Container>
+        </Container>
+      )}
     </>
   );
 };
